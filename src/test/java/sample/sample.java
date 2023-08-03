1502415bd55f85bd665d76efb9bc7f5fbd07d89f
Prasad desai <prasaddesai811@gmail.com>
2023-08-03 20:40:09 +0530

    sample commit

---
+++ src/test/java/sample/sample.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sample {
+
+	@Test
+	public void sampletest()
+	{
+		System.out.println("hi");
+		
+		System.out.println("Framework updated");
+	}
+	
+}
