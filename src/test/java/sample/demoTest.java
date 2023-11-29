d005822902e397e61545e7699e3d47e93bda82c2
prasaddesai95 <prasaddesai811@gmail.com>
2023-11-29 16:47:00 +0530

    newbuild1

--- src/test/java/sample/demoTest.java
+++ src/test/java/sample/demoTest.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 public class demoTest 
 {
 
-	@Test
+	@Test(groups = "smoke")
 	public void demopractice1Test()
 	{
 		System.out.println("--demo 1--");
 	}
 	
-	@Test
+	@Test(groups = "regression")
 	public void demopractice2Test()
 	{
 		System.out.println("--demo 2--");
--- src/test/java/sample/sampleTest.java
+++ src/test/java/sample/sampleTest.java
@@ -4,13 +4,13 @@ import org.testng.annotations.Test;
 
 public class sampleTest {
 
-	@Test
+	@Test(groups = "regression")
 	public void sample1Test()
 	{
 		System.out.println("--sample test2--");
 	}
 	
-	@Test
+	@Test(groups = {"smoke","regression"})
 	public void sample2Test()
 	{
 		System.out.println("--sample test2--");
